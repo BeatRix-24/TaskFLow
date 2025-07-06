735b7679cda1b64916cc6c5717abcba684b32052
BeatRix-24 <bhupendersaran2003@gmail.com>
2025-07-07 01:01:46 +0530

    Refactor

--- src/main/java/com/beatrix/to_do/service/JwtService.java
+++ src/main/java/com/beatrix/to_do/service/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "c3f1a9b27d4e5f68a9c0d1e2b3f4a5c6d7e8f90123456789abcdef0123456789";
+    @Value("${spring.jwt.secret}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token){
         return extractClaim(token,Claims::getSubject);
